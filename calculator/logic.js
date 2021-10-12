743eb1aeb1b6626bf44606b5946f1ddd9a4ad906
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-10-13 01:35:42 +0200

    mostly done

--- calculator/logic.js
+++ calculator/logic.js
@@ -1,10 +1,83 @@
-var f= document.getElementById("first");
-var s = document.getElementById("second");
 var result;
-function Sum()
+
+function Addition ()
 {
-    let first = parseInt(f);
-    let second = parseInt(s);
-    result = first + second;
-    document.getElementById("result").innerHTML = result;
+    let first = parseInt(document.getElementById("first").value);
+    let second = parseInt(document.getElementById("second").value);
+    if(!isNaN(first)&&!isNaN(second))
+    {
+        result = first + second;
+        document.getElementById("symbol").innerHTML = "+";
+        document.getElementById("result").innerHTML = "= " + result;
+    }
+    else
+        document.getElementById("result").innerHTML = "Only numbers allowed!!!";
+}
+
+function Subtraction()
+{
+    let first = parseInt(document.getElementById("first").value);
+    let second = parseInt(document.getElementById("second").value);
+    if(!isNaN(first)&&!isNaN(second))
+    {
+        result = first - second;
+        document.getElementById("symbol").innerHTML = "-";
+        document.getElementById("result").innerHTML = "= " + result;
+    }
+    else
+        document.getElementById("result").innerHTML = "Only numbers allowed!!!";
+}
+
+function Multiplication()
+{
+    let first = parseInt(document.getElementById("first").value);
+    let second = parseInt(document.getElementById("second").value);
+    if(!isNaN(first)&&!isNaN(second))
+    {
+        result = first * second;
+        document.getElementById("symbol").innerHTML = "*";
+        document.getElementById("result").innerHTML = "= " + result;
+    }
+    else
+        document.getElementById("result").innerHTML = "Only numbers allowed!!!";
+}
+
+function Division()
+{
+    let first = parseInt(document.getElementById("first").value);
+    let second = parseInt(document.getElementById("second").value);
+    if(!isNaN(first)&&!isNaN(second))
+        if(second!=0)
+        {
+            result = first / second;
+            document.getElementById("symbol").innerHTML = "/";
+            document.getElementById("result").innerHTML = "= " + result;
+        }
+        else
+            document.getElementById("result").innerHTML = "You can't divide by zero!!!";
+    else
+        document.getElementById("result").innerHTML = "Only numbers allowed!!!";
+}
+
+function Exponentiation()
+{
+    let first = parseInt(document.getElementById("first").value);
+    let second = parseInt(document.getElementById("second").value);
+    if(!isNaN(first)&&!isNaN(second))
+        if(second!=0)
+        {
+            result = first;
+            for(let i=0; i<second-1; i++)
+                result= result*first;
+            document.getElementById("symbol").innerHTML = "^";
+            document.getElementById("result").innerHTML = "= " + result;
+        }
+        else
+        {
+            result = 1
+            document.getElementById("symbol").innerHTML = "^";
+            document.getElementById("result").innerHTML = "= " + result;
+        }
+    else
+        document.getElementById("result").innerHTML = "Only numbers allowed!!!";
 }
