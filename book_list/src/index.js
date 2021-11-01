46091a2bb6fde5c55ec852e3b8f2b451e9b81e28
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-11-01 18:55:24 +0100

    .

--- book_list/src/index.js
+++ book_list/src/index.js
@@ -3,15 +3,7 @@ import reactDom from 'react-dom';
 
 function SomeBasicFunction()
 {
-  return(
-    <div>
-      <Message/>
-    </div>
-  );
-}
-
-const Message = () => {
-  return (<h1>Just done my first component</h1>);
+  return <h1>Just done my first component</h1>;
 }
 
 reactDom.render(<SomeBasicFunction/>, document.getElementById('root'))
